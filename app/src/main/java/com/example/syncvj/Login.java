377e2ddfa9c7472ff1ef32e896dde8aaac7fcbe1
ROHITH P R <rohith4pr@gmail.com>
2020-09-04 15:25:04 +0530

    Security Code Activity Is Added

--- app/src/main/java/com/example/syncvj/Login.java
+++ app/src/main/java/com/example/syncvj/Login.java
@@ -54,8 +54,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Intent intent = new Intent (Login.this, MainActivity2.class);
-                    intent.putExtra("ADMIN",1);
+                    Intent intent = new Intent (Login.this, SecurityCode.class);
                     startActivity(intent);
                 }
                 else
---
+++ app/src/main/java/com/example/syncvj/SecurityCode.java
@@ -0,0 +1,49 @@
+package com.example.syncvj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class SecurityCode extends AppCompatActivity implements View.OnClickListener {
+    EditText code;
+    String RC = "1234"; //This should be retrieved from the database and initial value should be null, NOTE : Datatype is made as String inorder to compare easily with EditText..
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_code);
+        code=findViewById(R.id.password_text);
+        findViewById(R.id.btLogin).setOnClickListener(this);
+    }
+    private void Login() {
+        String cd = code.getText().toString().trim();
+        if (cd.length() < 4) {
+            code.setError("Min Length is 4");
+            code.requestFocus();
+        }
+        if (cd.isEmpty()) {
+            code.setError("Code Required");
+            code.requestFocus();
+            return;
+        }
+        if(cd.equals(RC))//Please Verify The Datatype of both variables
+        {
+            Intent intent = new Intent (SecurityCode.this, MainActivity2.class);
+            intent.putExtra("ADMIN",1);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(SecurityCode.this,"Wrong Code, Try Again !!", Toast.LENGTH_SHORT).show();
+            code.requestFocus();
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btLogin) {
+            Login();
+        }
+    }
+}
